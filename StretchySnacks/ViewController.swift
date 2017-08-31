31033d6608e5eb589f115d5bd0da075c1134bdd0
nekomeo <linh.tu545@gmail.com>
2017-08-31 16:35:29 -0700

    Add image assets and stack view to nav bar

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -12,12 +12,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var navBarView: UIView!
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     @IBOutlet weak var plusButton: UIButton!
+    
+    var stackView: UIStackView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        horizonalStackView()
+        stackView.isHidden = true
     }
     
+    //MARK: Navigation Controller
     func navBarAutoLayoutConstraints() {
         
         UIView.animate(withDuration: 1.25,
@@ -29,9 +35,11 @@ class ViewController: UIViewController {
                             switch self.heightConstraint.constant {
                             case 200:
                                 self.heightConstraint.constant = 64
+                                
                                 break
                             case 64:
                                 self.heightConstraint.constant = 200
+                                
                                 break
                             default:
                                 break
@@ -43,22 +51,63 @@ class ViewController: UIViewController {
     
     func plusButtonRotation()
     {
+        var isOpen = Bool()
         UIView.animate(withDuration: 0.4, animations: {
             if (self.plusButton.transform == CGAffineTransform(rotationAngle: -CGFloat.pi/4))
             {
                 self.plusButton.transform = CGAffineTransform(rotationAngle: 0)
+                isOpen = false
             }
             else if (self.plusButton.transform == CGAffineTransform(rotationAngle: 0))
             {
                 self.plusButton.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/4)
+                isOpen = true
             }
         })
+        
+        if isOpen {
+            stackView.isHidden = false
+        } else {
+            stackView.isHidden = true
+        }
     }
 
 
     @IBAction func addButton(_ sender: UIButton) {
         navBarAutoLayoutConstraints()
         plusButtonRotation()
+    }
+    
+    //MARK: - Stackview
+    func horizonalStackView() {
+        let oreoView = UIImageView(image: #imageLiteral(resourceName: "oreos"))
+        let pizzaView = UIImageView(image: #imageLiteral(resourceName: "pizza_pockets"))
+        let poptartView = UIImageView(image: #imageLiteral(resourceName: "pop_tarts"))
+        let popsicleView = UIImageView(image: #imageLiteral(resourceName: "popsicle"))
+        let ramenView = UIImageView(image: #imageLiteral(resourceName: "ramen"))
+        
+        // set an aspect ratio
+        let oreoAspectRatio = NSLayoutConstraint(item: oreoView,
+                                                 attribute: .height,
+                                                 relatedBy: .equal,
+                                                 toItem: oreoView,
+                                                 attribute: .width,
+                                                 multiplier: 1,
+                                                 constant: 0)
+        oreoView.addConstraint(oreoAspectRatio)
+        
+        stackView = UIStackView(arrangedSubviews: [oreoView, pizzaView, poptartView, popsicleView, ramenView])
+
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        navBarView.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.leadingAnchor.constraint(equalTo: self.navBarView.leadingAnchor, constant: 0).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.navBarView.trailingAnchor, constant: 0).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.navBarView.bottomAnchor, constant: 0).isActive = true
     }
 }
 
